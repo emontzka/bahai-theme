6a9327f0cb6ff9e474c7fc003dbd827db4c0891b
Eric Montzka <emontzka@sbcglobal.net>
2020-08-28 12:07:03 -0500

    fix mobile layout

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 	<header id="masthead" class="site-header">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-sm-10 d-flex">
+				<div class="col-lg-10 d-flex">
 					<!-- <div class="row "> -->
 						<div class="site-branding">
 							<a class="custom-logo-link" href="<?php echo esc_url(
@@ -59,8 +59,11 @@
         'menu_id' => 'primary-menu',
     ]); ?>
 		</nav><!-- #site-navigation -->
+		<div class="navbar">
+					<div class="mobile-menu-button"><i class="fas fa-bars"></i><i class="fas fa-times"></i></div>
+				</div>
 			</div> <!--sm-md-10-->
-			<div class="col-sm-2">
+			<div class="col-lg-2">
 				<div class="d-none d-lg-block">
 				<ul class="secondary-nav">
 					<li data-toggle="modal" data-target="#SubscribeForm">Subscribe</li>
@@ -69,10 +72,7 @@
 					<!-- <i class="fas fa-lg fa-times"></i> -->
 				</ul>
 				</div>
-				<div class="navbar">
-					<div class="mobile-menu-button"><i class="fas fa-bars"></i><i class="fas fa-times"></i></div>
-<!-- <button class="navbar-toggler" type="button" data-togle="collapse" data-target="mobileNav" aria-controls="mobileNav" aria-expanded="false" aria-label="Toggle navigation">menu button</button> -->
-				</div>
+				
 
 			</div>
 			</div>
@@ -82,11 +82,6 @@
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-md-12">
-			<!-- <ul>
-				<li>Special Collections</li>
-				<li>Library </li>
-				<li>About</li>
-			</ul> -->
 		<?php wp_nav_menu([
         'theme_location' => 'menu-1',
         'menu_id' => 'primary-menu',
@@ -102,40 +97,5 @@
 				</div>
 			</div>
 		</div>
-		<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav> -->
 
 	</header><!-- #masthead -->
